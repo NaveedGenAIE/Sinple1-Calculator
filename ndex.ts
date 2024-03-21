9e9b2043a93a0dc152d99a1ad53d15c8ee06ac40
Naveed GAIE <160273630+NaveedGenAIE@users.noreply.github.com>
2024-03-22 00:46:46 +0500

    Initial commit

---
+++ ndex.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+    {message: "Enter First Number" , type:"number", name:"firstNumber"},
+    {message:"Enter Second Number" , type:"number", name:"secondNumber"},
+    {message:"Please Select One of the Operator ",
+    name:"operator",
+    type:"list",
+    choices:["Addition", "Subtraction", "Mutiplication", "Division"]
+},
+]);
+if (answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+}else if (answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+    
+}else if(answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}else if(answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+
+
+
+
